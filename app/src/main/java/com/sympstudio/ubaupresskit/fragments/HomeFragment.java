bae043f08fe4d779ca6bdb8854471e66327a83a3
serjaydc <serhiinote@gmail.com>
2026-06-29 10:36:38 +0100

    Home Page Fixes

--- app/src/main/java/com/sympstudio/ubaupresskit/fragments/HomeFragment.java
+++ app/src/main/java/com/sympstudio/ubaupresskit/fragments/HomeFragment.java
@@ -1,79 +1,15 @@
 package com.sympstudio.ubaupresskit.fragments;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.content.Intent;
+import android.net.Uri;
 
-import com.sympstudio.ubaupresskit.R;
-
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class HomeFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public HomeFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
-    }
-}
-
-package com.sympstudio.ubaupresskit.fragments;
-
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.Toast;
-
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.sympstudio.ubaupresskit.R;
@@ -84,19 +20,26 @@ public class HomeFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
 
-        // Inflate fragment_home.xml
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        // BUY NOW button
-        Button btnBuyNow = view.findViewById(R.id.btnBuyNow);
-        btnBuyNow.setOnClickListener(v -> {
-            // TODO: Add store link (Steam, PlayStation Store, etc.)
-            Toast.makeText(getContext(), "Redirecting to store...", Toast.LENGTH_SHORT).show();
-        });
+        TextView btnBuyNow = view.findViewById(R.id.btnBuyNow);
+
+        if (btnBuyNow != null) {
+            btnBuyNow.setOnClickListener(v -> {
+                String url = "https://your-store-link.com";
+
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+
+                startActivity(intent);
+            });
+        }
 
         return view;
     }
