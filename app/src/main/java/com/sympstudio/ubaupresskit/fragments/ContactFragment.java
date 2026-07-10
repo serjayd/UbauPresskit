f273928ff8663e7e81499e1ca6a1e13b0b5355d5
YuliiaHaponchak <ulaigaponchak@gmail.com>
2026-07-10 19:32:47 +0100

    changes

--- app/src/main/java/com/sympstudio/ubaupresskit/fragments/ContactFragment.java
+++ app/src/main/java/com/sympstudio/ubaupresskit/fragments/ContactFragment.java
@@ -14,6 +14,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+
 import com.sympstudio.ubaupresskit.DBHelper;
 import com.sympstudio.ubaupresskit.R;
 
