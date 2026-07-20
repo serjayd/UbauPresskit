af71cd8859ad00cfeded7f9d793b9713bce04afc
serjaydc <serhiinote@gmail.com>
2026-07-20 11:53:52 +0100

    Splash Screen Fixed Delay

--- app/src/main/java/com/sympstudio/ubaupresskit/SplashActivity.java
+++ app/src/main/java/com/sympstudio/ubaupresskit/SplashActivity.java
@@ -22,6 +22,6 @@ public class SplashActivity extends AppCompatActivity {
         new Handler(Looper.getMainLooper()).postDelayed(() -> {
             Intent intent = new Intent(SplashActivity.this, MainActivity.class);
             startActivity(intent);
-        }, 400);
+        }, 1000);
     }
 }
